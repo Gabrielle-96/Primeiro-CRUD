d2a98582409e075575e67ccc0a803d49cf0d6ef2
Gabrielle Martins da Silva <gabrielle.martins@tora.com.br>
2022-08-16 08:25:35 -0300

    first commit

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
-});
+    res.send("Hello World")
+  });
 
-app.listen(port, () => { 
+  app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
-});
+  });
