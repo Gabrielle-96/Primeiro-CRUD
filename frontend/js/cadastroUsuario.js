b6cc5f725b2f227c9aaa7bbf644955bc00de519c
Gabrielle Martins da Silva <gabrielle.martins@tora.com.br>
2022-08-18 14:29:02 -0300

    atualização frontend

--- frontend/js/cadastroUsuario.js
+++ frontend/js/cadastroUsuario.js
@@ -5,13 +5,19 @@ class Validator {
             'data-required',
             'data-min-length',
             'data-max-length',
+            'data-email-validate',
+            'data-only-letters',
+            'data-equal',
+            'data-password-validate',
+            
         ]
     }
 
     validate(form) {
 
         let currentValidations = document.querySelectorAll('form .error-validation');
-        if (currentValidations.length > 0) {
+
+        if(currentValidations.length > 0) {
             this.cleanValidations(currentValidations);
         }
 
@@ -20,8 +26,10 @@ class Validator {
         let inputsArray = [...inputs];
 
         inputsArray.forEach(function (input) {
+
             for (let r = 0; this.validations.length > r; r++) {
-                if (input.getAttribute(this.validations[r]) != null) {
+
+                if(input.getAttribute(this.validations[r]) != null) {
 
                     let method = this.validations[r].replace('data-', '').replace('-', '');
 
@@ -35,11 +43,12 @@ class Validator {
     }
 
     minlength(input, minValue) {
+
         let inputLength = input.value.length;
 
         let errorMessage = `O campo precisa ter pelo menos ${minValue} caracteres`;
 
-        if (inputLength < minValue) {
+        if(inputLength < minValue) {
             this.printMessage(input, errorMessage);
         }
     }
@@ -50,17 +59,85 @@ class Validator {
 
         let errorMessage = `O campo precisa ter menos que ${maxValue} caracteres`;
 
-        if (inputLength > maxValue) {
+        if(inputLength > maxValue) {
             this.printMessage(input, errorMessage);
         }
+    }
 
+    emailvalidate(input) {
+
+        let re = /\S+@\S+\.\S+/;
+
+        let email = input.value;
+
+        let errorMessage = `Insira um email válido (exemplo: email@email.com)`;
+
+        if(!re.test(email)) {
+            this.printMessage(input, errorMessage);
+        }
+    }
+
+    onlyletters(input) {
+
+        let re = /^[A-Za-z]+$/;
+
+        let inputValue = input.value;
+
+        let errorMessage = `Este campo deve conter apenas letras (A-Z, a-z)`;
+
+        if(!re.test(inputValue)) {
+            this.printMessage(input, errorMessage);
+        }
+    }
+
+    equal(input, inputName) {
+        let inputToCompare = document.getElementsByName(inputName)[0];
+
+        let errorMessage = `Este campo deve ser igual ao ${inputName}`;
+
+        if(input.value != inputToCompare.value) {
+            this.printMessage(input, errorMessage);
+        }
+    }
+
+    passwordvalidate(input) {
+
+        let arrayCaracter = input.value.split("");
+
+        let uppercases = 0;
+        let numbers = 0;
+
+        for(let i = 0; arrayCaracter.length > i; i++) {
+            if(arrayCaracter[i] === arrayCaracter[i].toUpperCase() && isNaN(parseInt(arrayCaracter[i]))){
+                uppercases++;
+            }else if(!isNaN(parseInt(arrayCaracter[i]))) {
+                numbers++;
+            }
+        }
+
+        if(uppercases === 0 || numbers === 0) {
+            let errorMessage = `A senha precisa conter um número e um caracter maiúsculo`;
+
+            this.printMessage(input, errorMessage);
+        }
+    }
+
+    required(input) {
+
+        let inputValue = input.value;
+
+        if (inputValue === '') {
+            let errorMessage = `Este campo é obrigatório`;
+
+            this.printMessage(input, errorMessage);
+        }
     }
 
     printMessage(input, msg) {
 
         let errorsQty = input.parentNode.querySelector('.error-validation');
-        if (errorsQty === null) {
 
+        if (errorsQty === null) {
             let template = document.querySelector('.error-validation').cloneNode(true);
 
             template.textContent = msg;
@@ -70,16 +147,6 @@ class Validator {
             template.classList.remove('template');
 
             inputParent.appendChild(template);
-        }
-    }
-
-    required(input) {
-        let inputValue = input.value;
-
-        if (inputValue === '') {
-            let errorMessage = `Este campo é obrigatório`;
-
-            this.printMessage(input, errorMessage);
         }
     }
 
