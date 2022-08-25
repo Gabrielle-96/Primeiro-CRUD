c053ba15f867db78e36d886c575b23ad638470d1
Gabrielle-96 <110261612+Gabrielle-96@users.noreply.github.com>
2022-08-25 11:29:08 -0300

    criação consultaUsuario.js

---
+++ frontend/js/consultaUsuario.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function (e) {
+    obterUsuarios();
+});
+
+function obterUsuarios() {
+    fetch('http://localhost:3000/usuario', {
+        method: 'GET'
+    })
+    .then(response => {
+        if (response.status == 200) {
+            console.log(response);
+            montarTabelaUsuarios(response);
+        } else {
+            alert("Ocorreu um problema ao obter usuários");
+        }
+    }).catch(function (res) {
+        alert("Erro ao obter usuários");
+        console.log(res);
+    });
+}
+
+function montarTabelaUsuarios(usuarios) {
+    let tabela = document.getElementById("usuarios");
+    usuarios.forEach(usuario => {
+        let linha = criarLinhaUsuario(usuario);
+        tabela.appendChild(linha);
+    });
+}
+
+function criarLinhaUsuario(usuario) {
+    console.log(usuario);
+
+    linha = document.createElement("tr");
+    tdId = document.createElement("td");
+    tdNome = document.createElement("td");
+    tdId.innerHTML = usuario.id;
+    tdNome.innerHTML = usuario.nome;
+
+    linha.appendChild(tdId);
+    linha.appendChild(tdNome);
+
+    return linha;
+}
