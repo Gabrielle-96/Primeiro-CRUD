c9a4417ff8a49e7610f88a986c92f831b57c962b
Gabrielle Martins da Silva <gabrielle.martins@tora.com.br>
2022-08-30 14:33:11 -0300

    create password-jwt.js and update loginController

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,13 +1,14 @@
 const database = require('../database/connections');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const passwordjwt = require('../database/password-jwt');
 
 class loginController {
 
     autenticar(req, res) {
         let { email, senha } = req.body;
         //const saltRounds = 10;
-        // senha = bcrypt.hashSync(senha, saltRounds);
+        //senha = bcrypt.hashSync(senha, saltRounds);
 
         database.select('id', 'nome', 'senha', 'email')
             .from('usuarios')
@@ -16,16 +17,15 @@ class loginController {
             .then(usuarios => {
                 if (usuarios.length > 0) {
                     if (bcrypt.compareSync(senha, usuarios[0].senha)) {
-                    // res.json(usuarios);
                     const token = jwt.sign({
                         id: usuarios[0].id,
                         nome: usuarios[0].nome,
                         senha: usuarios[0].senha,
                         email: usuarios[0].email
                     },
-                    "gf2xfsd54dvbdmeuxfd21s65d4an4give5frsadgdriofv2évb10c23v0b698d8f7g54dfg5d1asrsdnghvbcfd64ffg1315g4fg",
+                    passwordjwt,
                     {
-                        expiresIn: "1h"
+                        expiresIn: "5min"
                     });
 
                     res.json({
@@ -34,12 +34,11 @@ class loginController {
                         usuario: {
                             nome: usuarios[0].nome,
                             email: usuarios[0].email,
-                            senha: usuarios[0].senha,
                         }
                     });
 
                     } else {
-                        res.status(400).json({ message: "Falha na autenticação" });
+                        res.status(401).json({ message: "Falha na autenticação" });
                     }
                 } else {
                     res.status(400).json({ message: "Usuário não encontrado" });
---
+++ backend/database/password-jwt.js
@@ -0,0 +1,3 @@
+const passwordjwt = "secretpasswordtest";
+
+ module.exports = passwordjwt;
