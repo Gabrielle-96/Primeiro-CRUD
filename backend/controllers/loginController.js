236d9adc75bcc7061a14127c9103de35ab6f7cc2
Gabrielle Martins da Silva <gabrielle.martins@tora.com.br>
2022-08-30 12:06:22 -0300

    Inserindo jwt - loginController

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,5 +1,6 @@
 const database = require('../database/connections');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 class loginController {
 
@@ -8,16 +9,37 @@ class loginController {
         //const saltRounds = 10;
         // senha = bcrypt.hashSync(senha, saltRounds);
 
-        database.select('id', 'nome', 'senha')
+        database.select('id', 'nome', 'senha', 'email')
             .from('usuarios')
             .where('email', '=', email)
             //.where('senha', '=', senha)
             .then(usuarios => {
                 if (usuarios.length > 0) {
                     if (bcrypt.compareSync(senha, usuarios[0].senha)) {
-                        res.json(usuarios);
+                    // res.json(usuarios);
+                    const token = jwt.sign({
+                        id: usuarios[0].id,
+                        nome: usuarios[0].nome,
+                        senha: usuarios[0].senha,
+                        email: usuarios[0].email
+                    },
+                    "gf2xfsd54dvbdmeuxfd21s65d4an4give5frsadgdriofv2évb10c23v0b698d8f7g54dfg5d1asrsdnghvbcfd64ffg1315g4fg",
+                    {
+                        expiresIn: "1h"
+                    });
+
+                    res.json({
+                        message: "Autenticado com sucesso",
+                        token: token,
+                        usuario: {
+                            nome: usuarios[0].nome,
+                            email: usuarios[0].email,
+                            senha: usuarios[0].senha,
+                        }
+                    });
+
                     } else {
-                        res.status(400).json({ message: "E-mail ou senha incorretos" });
+                        res.status(400).json({ message: "Falha na autenticação" });
                     }
                 } else {
                     res.status(400).json({ message: "Usuário não encontrado" });
